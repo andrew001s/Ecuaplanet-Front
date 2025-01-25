c35412836957ae9b6275fe375a08e2e22c46928b
andrew001s <andresroman45678@gmail.com>
2025-01-25 11:54:25 -0500

    Agregar icono de tulipán y actualizar dependencias locales

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Material from "@expo/vector-icons/build/MaterialCommunityIcons";
 import './global.css';
 
 export default function App() {
   return (
     <View style={styles.container} className="bg-red-500">
       <Text>Open up App.js to start working on your app!</Text>
+      <Material name="flower-tulip" size={24} color="black" />
       <StatusBar style="auto" />
     </View>
   );
