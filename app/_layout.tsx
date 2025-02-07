6f63151a781ac53992b9433bc1e736f4738141d2
BrandPsycho <brand5961@gmail.com>
2025-02-06 23:01:54 -0500

    feat: :sparkles: Se agrego el archivo _layout como contenedor

---
+++ app/_layout.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Slot } from 'expo-router';
+
+const _layout = () => {
+  return (
+    <View>
+      <Text>_layout</Text>
+      <slot />
+    </View>
+  )
+}
+
+export default _layout
