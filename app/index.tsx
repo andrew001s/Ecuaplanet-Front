aa635eb9253f620b7f3c2104566ee7dd261cce73
BrandPsycho <brand5961@gmail.com>
2025-02-07 07:48:25 -0500

    fix: :bug: Se arreglo el problema del renderizado condicional

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,31 @@
-import { View, Text } from 'react-native'
-import React, { useEffect, useContext, useState } from 'react'
-import { useRouter } from 'expo-router';
+// index.tsx
+import { useEffect, useState, useContext } from "react";
+import { useRouter, useSegments, Redirect } from "expo-router"; // Importa useSegments
 import { AuthContext } from '../src/context/AuthContext';
+import { View, ActivityIndicator, Text } from 'react-native';
+import LoadingScreen from "../src/components/LoadingScreen";
 
-const index = () => {
-    const { isAuthenticated } = useContext(AuthContext);
-    const router = useRouter();
-    useEffect(() => {
-        setTimeout(() => {
-          router.replace('/LoginScreen'); // Redirige al login
-        }, 1000);
-      }, []);   
-  return (
-    <View>
-      <Text>index</Text>
-    </View>
-  )
-}
 
-export default index
+export default function Index() {
+  const router = useRouter();
+  const { isAuthenticated } = useContext(AuthContext); // Obtenemos si el usuario está autenticado
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Simular pantalla de carga por 1 segundo
+    const timer = setTimeout(() => {
+      if (isAuthenticated) {
+        router.replace("/HomeScreen"); // Si está autenticado, va al Home
+      } else {
+        router.replace("/LoginScreen"); // Si NO está autenticado, va al Login
+      }
+    }, 1200);
+
+    return () => clearTimeout(timer); // Limpiar el timer al desmontar
+  }, [isAuthenticated]);
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+  return null;
+};
