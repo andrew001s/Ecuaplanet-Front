066856c0d7c1311358ae71552e44f812d83ce05d
andrew001s <andresroman45678@gmail.com>
2025-02-07 10:16:47 -0500

    Revert "feat: :sparkles: Mejorar componente FaqList para manejar dinámicamente preguntas frecuentes según el tipo"
    This reverts commit 9b1e67f8c328a3bf78fd54f9cb1eab256014ae43.

--- src/components/FaqList.tsx
+++ src/components/FaqList.tsx
@@ -1,34 +1,16 @@
 import { View, Text, TouchableOpacity } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
-const initialFaqCultivo = 
-  [
-    'Resumen de cultivo',
-    'Cultivo de Freedom',
-    '¿Cuantas Variedades de flores hay?',
-  ]
-;
+const initialFaqCultivo = {
+  text: ['Resumen de cultivo', 'Cultivo de Freedom', '¿Cuantas Variedades de flores hay?'],
+};
 
-interface FaqListProps {
-  onSelectFaq: (text: string) => void;
-  type: string;
-}
-
-const FaqList = ({ onSelectFaq, type }: FaqListProps) => {
-  const generateKey = (message: string) =>
-    `${message}-${Math.random().toString(36).substr(2, 9)}`;
-  const [faq, setFaq] = useState<string[]>([]);
-
-  useEffect(() => {
-    if (type === 'cultivo') {
-      setFaq(initialFaqCultivo);
-    }
-  }
-    , [type]);
+const FaqList = ({ onSelectFaq }: { onSelectFaq: (text: string) => void }) => {
+  const generateKey = (message: string) => `${message}-${Math.random().toString(36).substr(2, 9)}`;
 
   return (
     <View className="flex-col mb-4 justify-start pr-2 items-start">
-      {faq.map((item) => (
+      {initialFaqCultivo.text.map((item) => (
         <TouchableOpacity
           key={generateKey(item)}
           className="bg-[#DEE1E6FF] m-2 border border-[#b5bac4] border-solid pr-6 pl-6 pt-3 pb-3 shadow-sm rounded-[16px]"
