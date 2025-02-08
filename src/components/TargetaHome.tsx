ea4e44c8d316e06f5e7cce2fddca3cbcb4d01a70
Carlos Oña <kaluxD21@hotmail.com>
2025-02-08 09:55:55 -0500

    feat: :sparkles: Cambio Targeta
    Cambio de la informacion presentada en la targeta

--- src/components/TargetaHome.tsx
+++ src/components/TargetaHome.tsx
@@ -5,12 +5,11 @@ import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 interface TargetaHomeProps{
   nombreCliente:string
   paisCliente:string
-  tipoCliente:string
-  idPedido:number
+  fechaVenta:Date;
   montoTotalVenta:number
 }
 
-export default function TargetaHome( {nombreCliente,paisCliente,tipoCliente,idPedido,montoTotalVenta}:TargetaHomeProps) {
+export default function TargetaHome( {nombreCliente,paisCliente,fechaVenta,montoTotalVenta}:TargetaHomeProps) {
   return (
     <View className="flex-1 p-3">
       <View>
@@ -23,10 +22,8 @@ export default function TargetaHome( {nombreCliente,paisCliente,tipoCliente,idPe
             />
           </View>
           <View className="flex-1 pl-10 pr-20">
-            <Text className="font-bold">Cliente: <Text className="font-normal">{nombreCliente}</Text></Text>
-            <Text className="font-bold">Tipo de Cliente: <Text className="font-normal">{tipoCliente}</Text></Text>
-            <Text className="font-bold">País: <Text className='font-normal'>{paisCliente}</Text></Text> 
-            <Text className="font-bold">Pedido: <Text className='font-normal'>#{idPedido}</Text></Text>
+            <Text className="font-bold">Venta a {nombreCliente}</Text>
+            <Text className="font-normal">{paisCliente} - {fechaVenta instanceof Date ? fechaVenta.toLocaleDateString() : fechaVenta}</Text> 
           </View>
           <View className="absolute top-3 right-3 pt-3">
             <Text className="text-green-500 font-bold">
