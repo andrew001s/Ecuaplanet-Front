14c47de90d8b8738df6cc5f0a0ed15cf7e447bac
andrew001s <andresroman45678@gmail.com>
2025-02-09 22:05:23 -0500

    feat: :sparkles: agregar funcionalidad de navegación en Chatheader

--- src/components/Chatheader.tsx
+++ src/components/Chatheader.tsx
@@ -1,11 +1,16 @@
 import { View, Text, TouchableOpacity } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { router } from 'expo-router';
 
 const Header = () => {
+  const handleBack = () => {
+    router.push('/HomeTemplate');
+  };
+
   return (
     <View className="pt-4 bg-white">
       <View className="relative flex-row items-center justify-center h-12">
-        <TouchableOpacity className="absolute left-4">
+        <TouchableOpacity className="absolute left-4" onPress={handleBack}>
           <Ionicons name="arrow-back" size={24} color="#9095A0FF" />
         </TouchableOpacity>
 
