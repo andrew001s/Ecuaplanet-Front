9b5abd2a03e633de703e49d7b9e215afa1ba59c1
andrew001s <andresroman45678@gmail.com>
2025-02-11 08:19:41 -0500

    feat: :sparkles: Ajuste del espaciado en la pantalla de inicio de sesión para mejorar la compatibilidad con iOS

--- app/LoginScreen.tsx
+++ app/LoginScreen.tsx
@@ -48,7 +48,7 @@ const LoginScreen = () => {
                         onChangeText={setEmail}
                         autoCapitalize="none"
                     />
-                    <Text className="text-lg text-start mt-5 py-2 font-bold">Contraseña</Text>
+                    <Text className={`text-lg text-start  ${Platform.OS==='ios' ? 'pt-7  ':'mt-5 py-2'} font-bold`}>Contraseña</Text>
                     <View className="relative"> 
                         <TextInput
                             className={`text-lg border-b border-gray-300 ${Platform.OS==='ios' ? 'pt-5 pb-4 ':''} focus:border-b-[#379AE6] px-4 pr-10`} 
